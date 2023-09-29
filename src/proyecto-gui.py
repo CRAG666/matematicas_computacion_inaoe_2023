d5441dce24245d7045a141cd7b7f3f04781448de
Crag <ogeidca49@gmail.com>
2023-09-28 21:54:36 -0600

    Por fin termine

--- src/proyecto-gui.py
+++ src/proyecto-gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image, ImageTk
 from sklearn.decomposition import TruncatedSVD
@@ -11,57 +12,100 @@ class ImageCompressionApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Image Compression with SVD")
-        self.root.geometry(
-            "800x400"
-        )  # Establece un tamaño predeterminado para la ventana
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        self.root.geometry(f"{screen_width}x{screen_height}")
 
-        # Crear el marco principal
         self.frame = ttk.Frame(root)
-        self.frame.grid(row=0, column=0, padx=10, pady=10)
+        self.frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-        # Botones
-        self.load_button = ttk.Button(
-            self.frame, text="Cargar Imagen", command=self.load_image
-        )
-        self.load_button.grid(row=0, column=0, pady=10)
+        self.create_columns()
 
-        self.svd_button = ttk.Button(
-            self.frame, text="Comprimir", command=self.on_compress_image
-        )
-        self.svd_button.grid(row=0, column=2, pady=10)
-        self.svd_button.state(["disabled"])
-
-        self.restore_button = ttk.Button(
-            self.frame, text="Restaurar", command=self.restore_image
-        )
-        self.restore_button.grid(row=0, column=3, pady=10)
-        self.restore_button.state(["disabled"])
-
-        # Lienzos para mostrar imágenes
-        self.canvas_original = tk.Canvas(self.frame, width=256, height=256)
-        self.canvas_original.grid(row=1, column=0, padx=5)
-
-        self.canvas_original_components = tk.Canvas(self.frame, width=256, height=256)
-        self.canvas_original_components.grid(row=1, column=1, padx=5)
-
-        self.canvas_compressed = tk.Canvas(self.frame, width=256, height=256)
-        self.canvas_compressed.grid(row=1, column=2, padx=5)
-
-        self.canvas_decompressed = tk.Canvas(self.frame, width=256, height=256)
-        self.canvas_decompressed.grid(row=1, column=3, padx=5)
-
-        # Etiqueta y entrada para el número de componentes
-        self.n_components_label = ttk.Label(self.frame, text="Número de Componentes:")
-        self.n_components_label.grid(row=2, column=0, pady=5)
-
-        self.n_components_entry = ttk.Entry(self.frame)
-        self.n_components_entry.grid(row=2, column=1, pady=5)
-        self.n_components_entry.insert(0, "10")
-
-        # Cargar el clasificador de detección de caras de OpenCV
         self.face_cascade = cv2.CascadeClassifier(
             cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
         )
+
+    def create_columns(self):
+        self.create_buttons()
+
+        # Columna 1: Original
+        self.column1 = ttk.Frame(self.frame)
+        self.column1.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
+
+        self.canvas_original = tk.Canvas(self.column1, width=256, height=256)
+        self.canvas_original.grid(row=0, column=0, padx=5)
+
+        self.canvas_original_components = tk.Canvas(self.column1, width=256, height=256)
+        self.canvas_original_components.grid(row=0, column=1, padx=5)
+
+        # Columna 2: Comprimido
+        self.column2 = ttk.Frame(self.frame)
+        self.column2.grid(row=1, column=1, padx=5, pady=5, sticky="nsew")
+
+        self.canvas_compressed = tk.Canvas(self.column2, width=256, height=256)
+        self.canvas_compressed.grid(row=0, column=0, padx=5)
+
+        # Columna 3: Restaurado
+        self.column3 = ttk.Frame(self.frame)
+        self.column3.grid(row=1, column=2, padx=5, pady=5, sticky="nsew")
+
+        self.canvas_decompressed = tk.Canvas(self.column3, width=256, height=256)
+        self.canvas_decompressed.grid(row=0, column=0, padx=5)
+
+        # Columna 4: Gráficos
+        self.column4 = ttk.Frame(self.frame)
+        self.column4.grid(row=1, column=3, padx=5, pady=5, sticky="nsew")
+
+        self.explained_variance_canvas = tk.Canvas(self.column4, width=400, height=300)
+        self.explained_variance_canvas.grid(row=0, column=0, padx=5, pady=5)
+
+        self.reconstruction_error_canvas = tk.Canvas(
+            self.column4, width=400, height=300
+        )
+        self.reconstruction_error_canvas.grid(row=1, column=0, padx=5, pady=5)
+
+        # Columna 5: histogramas
+        self.column5 = ttk.Frame(self.frame)
+        self.column5.grid(row=1, column=4, padx=5, pady=5, sticky="nsew")
+
+        self.histogram_canvas = tk.Canvas(self.column5, width=400, height=300)
+        self.histogram_canvas.grid(row=0, column=0, padx=5, pady=5)
+
+        self.histogram_reconstruction_canvas = tk.Canvas(
+            self.column5, width=400, height=300
+        )
+        self.histogram_reconstruction_canvas.grid(row=1, column=0, padx=5, pady=5)
+
+    def create_buttons(self):
+        self.button_frame = ttk.Frame(self.frame)
+        self.button_frame.grid(
+            row=0, column=0, padx=5, pady=10, columnspan=5, sticky="w"
+        )
+
+        self.load_button = ttk.Button(
+            self.button_frame, text="Cargar Imagen", command=self.load_image
+        )
+        self.load_button.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+
+        self.n_components_label = ttk.Label(
+            self.button_frame, text="Número de Componentes:"
+        )
+        self.n_components_label.grid(row=0, column=1, padx=5, pady=5, sticky="w")
+
+        self.n_components_entry = ttk.Entry(self.button_frame)
+        self.n_components_entry.grid(row=0, column=2, padx=5, pady=5, sticky="w")
+
+        self.svd_button = ttk.Button(
+            self.button_frame, text="Comprimir", command=self.on_compress_image
+        )
+        self.svd_button.grid(row=0, column=3, padx=5, pady=5, sticky="w")
+        self.svd_button.state(["disabled"])
+
+        self.restore_button = ttk.Button(
+            self.button_frame, text="Restaurar", command=self.restore_image
+        )
+        self.restore_button.grid(row=0, column=4, padx=5, pady=5, sticky="w")
+        self.restore_button.state(["disabled"])
 
     def load_image(self):
         file_path = filedialog.askopenfilename(
@@ -74,9 +118,7 @@ class ImageCompressionApp:
                 0, 0, anchor="nw", image=self.photo_original
             )
             self.svd_button.state(["!disabled"])
-            self.image_data = np.array(
-                self.image.convert("L")
-            )  # Convertir a escala de grises
+            self.image_data = np.array(self.image.convert("L"))
             n_components = min(self.image_data.shape)
             compressed_image = self.compress_image(n_components)
             self.photo_org = ImageTk.PhotoImage(compressed_image)
@@ -84,7 +126,7 @@ class ImageCompressionApp:
                 0,
                 0,
                 anchor="nw",
-                image=self.photo_org,  # Mostrar la imagen comprimida aquí
+                image=self.photo_org,
             )
 
     def on_compress_image(self):
@@ -97,17 +139,20 @@ class ImageCompressionApp:
             0,
             0,
             anchor="nw",
-            image=self.photo_compressed,  # Mostrar la imagen comprimida aquí
+            image=self.photo_compressed,
         )
+
+        explained_variance = self.svd.explained_variance_ratio_
+        x_values = list(range(1, n_components + 1))
+
+        self.plot_explained_variance(x_values, explained_variance)
+        self.plot_reconstruction_error(x_values)
 
     def compress_image(self, n_components):
         if hasattr(self, "image"):
             try:
-                # Realizar la descomposición SVD
                 self.svd = TruncatedSVD(n_components=n_components)
                 self.components = self.svd.fit_transform(self.image_data)
-
-                # Habilitar el botón de descompresión
                 self.restore_button.state(["!disabled"])
                 return self.get_compressed_components(self.components)
             except ValueError as e:
@@ -115,16 +160,12 @@ class ImageCompressionApp:
 
     def get_compressed_components(self, components):
         num_components = components.shape[1]
-
         noise_size = (256, 256)
-
         noise_image = np.random.randint(0, 256, size=noise_size, dtype=np.uint8)
         noise_image = Image.fromarray(noise_image, "L")
-
         component_images = []
 
         for i in range(num_components):
-            # Escalar los valores de los componentes para que estén en el rango 0-255
             component_data = components[:, i]
             component_min = component_data.min()
             component_max = component_data.max()
@@ -158,31 +199,95 @@ class ImageCompressionApp:
     def restore_image(self):
         if hasattr(self, "svd") and self.svd is not None:
             try:
-                # Reconstruye la matriz original a partir de los componentes y los valores singulares
-                reconstructed_image_data = np.dot(self.components, self.svd.components_)
-
-                # Asegúrate de que los valores estén en el rango adecuado (0-255)
-                reconstructed_image_data = np.clip(reconstructed_image_data, 0, 255)
-
-                # Redimensiona la matriz a la forma original de la imagen
-                reconstructed_image_data = reconstructed_image_data.reshape(
-                    self.image.size[1], self.image.size[0]
-                )
-
-                # Convierte la matriz reconstruida en una imagen
-                reconstructed_image_data = np.uint8(reconstructed_image_data)
-                decompressed_image = Image.fromarray(reconstructed_image_data)
-
-                # Reescala la imagen reconstruida para que tenga el mismo tamaño que la original
-                decompressed_image = decompressed_image.resize(self.image.size)
-
-                self.photo_decompressed = ImageTk.PhotoImage(decompressed_image)
+                reconstructed_image_data = self.svd.inverse_transform(self.components)
+                reconstructed_image = np.uint8(reconstructed_image_data)
+                reconstructed_image = Image.fromarray(reconstructed_image_data)
+                reconstructed_image = reconstructed_image.resize(self.image.size)
+                self.photo_decompressed = ImageTk.PhotoImage(reconstructed_image)
                 self.canvas_decompressed.create_image(
                     0, 0, anchor="nw", image=self.photo_decompressed
                 )
+
+                self.plot_histogram(self.image_data)
+                self.plot_histogram_reconstruction(reconstructed_image_data)
             except Exception as e:
                 messagebox.showerror("Error", str(e))
                 self.restore_button.state(["disabled"])
+
+    def plot_explained_variance(self, x_values, explained_variance):
+        plt.figure(figsize=(6, 4))
+        plt.plot(x_values, explained_variance, marker="o", linestyle="-")
+        plt.title("Varianza Explicada por Componente Principal")
+        plt.xlabel("Número de Componentes Principales")
+        plt.ylabel("Varianza Explicada")
+        plt.grid()
+        plt.tight_layout()
+
+        self.explained_variance_canvas.delete("all")
+        self.plot_to_canvas(self.explained_variance_canvas, plt)
+
+    def plot_reconstruction_error(self, x_values):
+        reconstruction_error = self.calculate_reconstruction_error(
+            len(x_values), x_values
+        )
+        plt.figure(figsize=(6, 4))
+        plt.plot(x_values, reconstruction_error, marker="o", linestyle="-")
+        plt.title("Error de Reconstrucción")
+        plt.xlabel("Número de Componentes Principales")
+        plt.ylabel("Error")
+        plt.grid()
+        plt.tight_layout()
+
+        self.reconstruction_error_canvas.delete("all")
+        self.plot_to_canvas(self.reconstruction_error_canvas, plt)
+
+    def plot_histogram(self, image_data):
+        plt.figure(figsize=(6, 4))
+        plt.hist(image_data.ravel(), bins=256, range=(0, 256), density=True)
+        plt.title("Histograma de la Imagen Original")
+        plt.xlabel("Valor de Pixel")
+        plt.ylabel("Frecuencia Normalizada")
+        plt.grid()
+        plt.tight_layout()
+
+        self.histogram_canvas.delete("all")
+        self.plot_to_canvas(self.histogram_canvas, plt)
+
+    def plot_histogram_reconstruction(self, image_data):
+        plt.figure(figsize=(6, 4))
+        plt.hist(image_data.ravel(), bins=256, range=(0, 256), density=True)
+        plt.title("Histograma de la Imagen Reconstruida")
+        plt.xlabel("Valor de Pixel")
+        plt.ylabel("Frecuencia Normalizada")
+        plt.grid()
+        plt.tight_layout()
+
+        self.histogram_reconstruction_canvas.delete("all")
+        self.plot_to_canvas(self.histogram_reconstruction_canvas, plt)
+
+    def calculate_reconstruction_error(self, n_components, x_values):
+        svd = TruncatedSVD(n_components=n_components)
+        components = svd.fit_transform(self.image_data)
+
+        reconstruction_error = []
+
+        for i in range(1, n_components + 1):
+            reconstructed_image_data = np.dot(components[:, :i], svd.components_[:i, :])
+            error = np.linalg.norm(self.image_data - reconstructed_image_data)
+            reconstruction_error.append(error)
+
+        return reconstruction_error
+
+    def plot_to_canvas(self, canvas, plt):
+        plt.savefig("temp_plot.png", bbox_inches="tight", format="png", dpi=100)
+        plt.close()
+
+        image = Image.open("temp_plot.png")
+        image = image.resize((canvas.winfo_width(), canvas.winfo_height()))
+        photo = ImageTk.PhotoImage(image)
+
+        canvas.create_image(0, 0, anchor="nw", image=photo)
+        canvas.image = photo
 
 
 if __name__ == "__main__":
--- src/proyecto.py
+++ src/proyecto.py
@@ -1,46 +1,173 @@
+import csv
+import random
+
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.sparse import csr_matrix
+from skimage.measure import shannon_entropy
+from skimage.metrics import structural_similarity as ssim
+from skimage.transform import resize  # Importar la función resize
 from sklearn.datasets import fetch_olivetti_faces
-from sklearn.utils.extmath import randomized_svd
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-# Cargar el conjunto de datos de caras Olivetti
-faces_data = fetch_olivetti_faces(shuffle=True, random_state=42)
-images = faces_data.images
-n_samples, h, w = images.shape
-X = images.reshape((n_samples, -1))
 
-# Convertir la matriz de imágenes en una matriz dispersa
-sparse_X = csr_matrix(X)
+class ImageCompressionApp:
+    def __init__(self):
+        self.data = fetch_olivetti_faces(shuffle=True, random_state=42)
+        self.images = self.data.images
+        self.target = self.data.target
+        self.n_samples, self.n_features = self.data.data.shape
+        self.n_components = 0
+        self.svd = None
 
-# Realizar la descomposición SVD en la matriz dispersa
-U, S, VT = randomized_svd(sparse_X, n_components=150, random_state=42)
+    def compress_images(self, n_components):
+        if n_components <= 0:
+            raise ValueError("El número de componentes debe ser mayor que 0.")
+        self.n_components = n_components
+        self.svd = TruncatedSVD(n_components=n_components)
+        self.images_compressed = self.svd.fit_transform(self.data.data)
+        self.reconstructed_images = self.svd.inverse_transform(self.images_compressed)
 
-# Reconstruir las imágenes originales a partir de VT
-reconstructed_faces = np.dot(U, np.dot(np.diag(S), VT))
-reconstructed_faces = reconstructed_faces.reshape((n_samples, h, w))
+    def _check_compression_state(self):
+        if self.n_components <= 0 or self.svd is None:
+            raise ValueError("Primero debes comprimir las imágenes.")
 
-# Visualizar algunas de las imágenes originales y reconstruidas
-n_images_to_display = 4
-plt.figure(figsize=(15, 6))
-for i in range(n_images_to_display):
-    plt.subplot(4, n_images_to_display, i + 1)
-    plt.imshow(images[i], cmap="gray")
-    plt.title("Original")
+    def convert_images_to_jpg_format(self, image):
+        self._check_compression_state()
+        return (image * 255).astype(np.uint8)
 
-    plt.subplot(4, n_images_to_display, i + n_images_to_display + 1)
-    plt.imshow(sparse_X[i].toarray().reshape(h, w), cmap="binary")
-    plt.title("Imagen dispersa")
+    def calculate_error(self):
+        self._check_compression_state()
+        return mean_squared_error(self.data.data, self.reconstructed_images)
 
-    plt.subplot(4, n_images_to_display, i + 2 * n_images_to_display + 1)
-    u_image = np.dot(U[i], VT)  # Reconstruir la imagen de U usando U[i] y VT
-    u_image = u_image.reshape(h, w)
-    plt.imshow(u_image, cmap="gray")
-    plt.title("Imagen de U")
+    def analyze_images(self):
+        self._check_compression_state()
+        num_images_to_analyze = len(self.data.images)
+        metrics = []
 
-    plt.subplot(4, n_images_to_display, i + 3 * n_images_to_display + 1)
-    plt.imshow(reconstructed_faces[i], cmap="gray")
-    plt.title("Reconstruido")
+        # Redimensionar todas las imágenes reconstruidas al tamaño de la primera imagen original
+        reference_shape = self.data.images[0].shape
 
-plt.tight_layout()
-plt.show()
+        for i in range(num_images_to_analyze):
+            mse = mean_squared_error(
+                self.data.images[i].ravel(),
+                self.reconstructed_images[i].ravel(),
+            )
+            mae = mean_absolute_error(
+                self.data.images[i].ravel(),
+                self.reconstructed_images[i].ravel(),
+            )
+            r2 = r2_score(
+                self.data.images[i].ravel(),
+                self.reconstructed_images[i].ravel(),
+            )
+            psnr = 20 * np.log10(255.0 / np.sqrt(mse))
+
+            # Redimensionar la imagen reconstruida al tamaño de la referencia
+            reconstructed_image_resized = resize(
+                self.reconstructed_images[i], reference_shape, anti_aliasing=True
+            )
+
+            image_ssim = ssim(
+                self.data.images[i],
+                reconstructed_image_resized,
+                data_range=self.data.images[i].max() - self.data.images[i].min(),
+            )
+            entropy_original = shannon_entropy(self.data.images[i])
+            entropy_reconstructed = shannon_entropy(reconstructed_image_resized)
+
+            metrics.append(
+                {
+                    "Imagen": i + 1,
+                    "MSE": mse,
+                    "MAE": mae,
+                    "R^2": r2,
+                    "PSNR": psnr,
+                    "SSIM": image_ssim,
+                    "Entropy_Original": entropy_original,
+                    "Entropy_Reconstructed": entropy_reconstructed,
+                }
+            )
+
+        return metrics
+
+    def plot_images_with_metrics(self, num_images_to_show=4):
+        if num_images_to_show <= 0:
+            raise ValueError("El número de imágenes a mostrar debe ser mayor que 0.")
+
+        random_indices = random.sample(range(len(self.data.images)), num_images_to_show)
+
+        plt.figure(figsize=(15, 12))
+
+        for i, idx in enumerate(random_indices):
+            plt.subplot(4, num_images_to_show, i + 1)
+            plt.imshow(self.data.images[idx], cmap=plt.cm.gray)
+            plt.title(f"Imagen {idx + 1}")
+            plt.axis("off")
+
+            image_jpg = self.convert_images_to_jpg_format(self.data.images[idx])
+            plt.subplot(4, num_images_to_show, i + num_images_to_show + 1)
+            plt.hist(
+                image_jpg.ravel(),
+                bins=256,
+                range=(0, 256),
+                density=True,
+                color="b",
+                alpha=0.7,
+            )
+            plt.title(f"Histograma {idx + 1}")
+            plt.xlabel("Valor de Pixel")
+            plt.ylabel("Frecuencia Normalizada")
+
+            plt.subplot(4, num_images_to_show, i + 2 * num_images_to_show + 1)
+            plt.imshow(
+                self.reconstructed_images[idx].reshape(self.data.images[0].shape),
+                cmap=plt.cm.gray,
+            )
+            plt.title(f"Reconstruida {idx + 1}")
+            plt.axis("off")
+
+            image_reconstructed_jpg = self.convert_images_to_jpg_format(
+                self.reconstructed_images[idx]
+            )
+            plt.subplot(4, num_images_to_show, i + 3 * num_images_to_show + 1)
+            plt.hist(
+                image_reconstructed_jpg.ravel(),
+                bins=256,
+                range=(0, 256),
+                density=True,
+                color="r",
+                alpha=0.7,
+            )
+            plt.title(f"Histograma Reconstruido {idx + 1}")
+            plt.xlabel("Valor de Pixel")
+            plt.ylabel("Frecuencia Normalizada")
+
+        plt.tight_layout()
+        plt.show()
+
+
+if __name__ == "__main__":
+    app = OlivettiFacesApp()
+    num_components = 225
+    app.compress_images(num_components)
+
+    metrics_list = app.analyze_images()
+
+    with open("metrics.csv", "w", newline="") as csvfile:
+        fieldnames = [
+            "Imagen",
+            "MSE",
+            "MAE",
+            "R^2",
+            "PSNR",
+            "SSIM",
+            "Entropy_Original",
+            "Entropy_Reconstructed",
+        ]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(metrics_list)
+
+    app.plot_images_with_metrics(num_images_to_show=4)
